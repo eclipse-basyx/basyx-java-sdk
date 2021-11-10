bcf61e665d7edf6acf058e716d84df3271965640
Mohammad Ghazanfar Ali Danish <mdanish98@gmail.com>
2021-11-10 01:31:48 +0100

    Fixed bug of Reference.isReference throws Exception when working with raw maps

--- src/test/java/org/eclipse/basyx/testsuite/regression/submodel/metamodel/map/reference/TestReference.java
+++ src/test/java/org/eclipse/basyx/testsuite/regression/submodel/metamodel/map/reference/TestReference.java
@@ -27,6 +27,7 @@ import org.eclipse.basyx.submodel.metamodel.map.qualifier.Identifiable;
 import org.eclipse.basyx.submodel.metamodel.map.qualifier.LangStrings;
 import org.eclipse.basyx.submodel.metamodel.map.reference.Key;
 import org.eclipse.basyx.submodel.metamodel.map.reference.Reference;
+import org.eclipse.basyx.vab.support.TypeDestroyer;
 import org.junit.Test;
 
 
@@ -96,8 +97,14 @@ public class TestReference {
 		
 		assertTrue(Reference.isReference(reference));
 		
+		//Added new test case for java.util.HashMap cannot be cast to org.eclipse.basyx.submodel.metamodel.map.reference.Key
+		assertTrue(Reference.isReference(TypeDestroyer.destroyType(reference)));
+		
 		reference.put(Reference.KEY, "nonsense");
 		
 		assertFalse(Reference.isReference(reference));
+		
+		//Added new test case for java.util.HashMap cannot be cast to org.eclipse.basyx.submodel.metamodel.map.reference.Key
+		assertFalse(Reference.isReference(TypeDestroyer.destroyType(reference)));
 	}
 }
