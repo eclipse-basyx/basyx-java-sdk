46d30399db00dd2a39d9c6418d6c402606bdfa2c
Mohammad Ghazanfar Ali Danish <62088117+mdanish98@users.noreply.github.com>
2021-11-19 11:54:00 +0100

    Moved redundant codes of MQTT in helper classes. (#16)
    * Added helper classes for MqttAASRegistry, MqttAASAggregator, MqttSubmodelAPI
    
    * Added helper class definitions

---
+++ src/main/java/org/eclipse/basyx/extensions/aas/aggregator/mqtt/MqttAASAggregatorHelper.java
@@ -0,0 +1,22 @@
+/*******************************************************************************
+ * Copyright (C) 2021 the Eclipse BaSyx Authors
+ * 
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at https://www.eclipse.org/legal/epl-2.0/
+ * 
+ * SPDX-License-Identifier: EPL-2.0
+ ******************************************************************************/
+package org.eclipse.basyx.extensions.aas.aggregator.mqtt;
+
+/**
+ * A helper class containing string constants of topics used by the AASAggregator. 
+ * 
+ * @author danish
+ *
+ */
+public class MqttAASAggregatorHelper {
+	public static final String TOPIC_CREATEAAS = "BaSyxAggregator_createdAAS";
+	public static final String TOPIC_DELETEAAS = "BaSyxAggregator_deletedAAS";
+	public static final String TOPIC_UPDATEAAS = "BaSyxAggregator_updatedAAS";
+}
