43fba511da87362d47fa6eadf1a881105993ebef
Mohammad Ghazanfar Ali Danish <62088117+mdanish98@users.noreply.github.com>
2021-11-23 15:41:03 +0100

    Added functionality in convertCollectionToIDMap that throws the IdShortDuplicationException (#24)
    Signed-off-by: Mohammad Ghazanfar Ali Danish <ghazanfar.danish@iese.fraunhofer.de>

--- src/test/java/org/eclipse/basyx/testsuite/regression/submodel/metamodel/map/TestSubmodel.java
+++ src/test/java/org/eclipse/basyx/testsuite/regression/submodel/metamodel/map/TestSubmodel.java
@@ -11,10 +11,11 @@ package org.eclipse.basyx.testsuite.regression.submodel.metamodel.map;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
-
+import java.util.Arrays;
+import java.util.Collection;
 import java.util.Collections;
 import java.util.Map;
-
+import org.eclipse.basyx.aas.metamodel.exception.IdShortDuplicationException;
 import org.eclipse.basyx.aas.metamodel.map.descriptor.ModelUrn;
 import org.eclipse.basyx.submodel.metamodel.api.ISubmodel;
 import org.eclipse.basyx.submodel.metamodel.api.identifier.IIdentifier;
@@ -83,6 +84,30 @@ public class TestSubmodel extends TestSubmodelSuite {
 
 	@Override
 	protected ISubmodel getSubmodel() {
+		return submodel;
+	}
+	
+	@Test(expected = IdShortDuplicationException.class)
+	public void checkForExceptionWithDuplicateIdShortInSubmodel() {
+		Map<String, Object> faultySubmodel = createSubmodelWithDuplicateIdShortProperties();
+
+		Submodel.createAsFacade(faultySubmodel);
+	}
+
+	private Map<String, Object> createSubmodelWithDuplicateIdShortProperties() {
+		String duplicateIdShort = "testProp";
+		
+		Property property1 = new Property(duplicateIdShort, 5);
+		Property property2 = new Property(duplicateIdShort, 7);
+		
+		Collection<Map<String, Object>> collection = Arrays.asList(property1, property2);
+		
+		String idShort = "submodelIdShort";
+		
+		Submodel submodel = new Submodel(idShort, new Identifier(IdentifierType.IRI, idShort));
+		
+		submodel.put(Submodel.SUBMODELELEMENT, collection);
+		
 		return submodel;
 	}
 }
