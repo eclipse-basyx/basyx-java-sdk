dc482f1607b1938f63a7a34eede3d0e559d433a3
Jannis Jung <jannis.jung@iese.fraunhofer.de>
2022-01-12 10:17:15 +0100

    Reworks test to incorporate parent shell id checks
    Co-authored-by: Rene Fischer <rene-pascal.fischer@iese.fraunhofer.de>
    Signed-off-by: Jannis Jung <jannis.jung@iese.fraunhofer.de>

--- src/test/java/org/eclipse/basyx/testsuite/regression/submodel/aggregator/observing/ObservableSubmodelAggregatorTest.java
+++ src/test/java/org/eclipse/basyx/testsuite/regression/submodel/aggregator/observing/ObservableSubmodelAggregatorTest.java
@@ -20,8 +20,11 @@ import org.eclipse.basyx.submodel.aggregator.api.ISubmodelAggregator;
 import org.eclipse.basyx.submodel.aggregator.observing.ISubmodelAggregatorObserver;
 import org.eclipse.basyx.submodel.aggregator.observing.ObservableSubmodelAggregator;
 import org.eclipse.basyx.submodel.metamodel.api.identifier.IdentifierType;
+import org.eclipse.basyx.submodel.metamodel.api.reference.IReference;
+import org.eclipse.basyx.submodel.metamodel.api.reference.enums.KeyElements;
 import org.eclipse.basyx.submodel.metamodel.map.Submodel;
 import org.eclipse.basyx.submodel.metamodel.map.identifier.Identifier;
+import org.eclipse.basyx.submodel.metamodel.map.reference.Reference;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -37,6 +40,8 @@ public class ObservableSubmodelAggregatorTest {
 	private static final String SUBMODEL_IDSHORT = "submodelIdShort";
 	private static final String SUBMODEL_ID = "submodelId";
 	private static final Identifier SUBMODEL_IDENTIFIER = new Identifier(IdentifierType.IRI, SUBMODEL_ID);
+	private static final String PARENT_AAS_ID = "parent_aas";
+	private static final Identifier PARENT_AAS_IDENTIFIER = new Identifier(IdentifierType.IRI, PARENT_AAS_ID);
 
 	private ObservableSubmodelAggregator observerdSubmodelAggregator;
 	private MockObserver observer;
@@ -45,6 +50,10 @@ public class ObservableSubmodelAggregatorTest {
 	public void setup() {
 		ISubmodelAggregator aggregator = new SubmodelAggregator();
 		submodel = new Submodel(SUBMODEL_IDSHORT, SUBMODEL_IDENTIFIER);
+
+		IReference parentReference = new Reference(PARENT_AAS_IDENTIFIER, KeyElements.ASSETADMINISTRATIONSHELL, false);
+
+		submodel.setParent(parentReference);
 		aggregator.createSubmodel(submodel);
 
 		observerdSubmodelAggregator = new ObservableSubmodelAggregator(aggregator);
@@ -74,6 +83,7 @@ public class ObservableSubmodelAggregatorTest {
 
 		assertTrue(observer.updatedNotified);
 		assertEquals(SUBMODEL_ID, observer.submodelId);
+		assertEquals(PARENT_AAS_ID, observer.shellId);
 	}
 
 	@Test
@@ -81,6 +91,7 @@ public class ObservableSubmodelAggregatorTest {
 		observerdSubmodelAggregator.deleteSubmodelByIdentifier(SUBMODEL_IDENTIFIER);
 		assertTrue(observer.deletedNotified);
 		assertEquals(SUBMODEL_ID, observer.submodelId);
+		assertEquals(PARENT_AAS_ID, observer.shellId);
 	}
 
 	@Test
@@ -88,6 +99,7 @@ public class ObservableSubmodelAggregatorTest {
 		observerdSubmodelAggregator.deleteSubmodelByIdShort(SUBMODEL_IDSHORT);
 		assertTrue(observer.deletedNotified);
 		assertEquals(SUBMODEL_ID, observer.submodelId);
+		assertEquals(PARENT_AAS_ID, observer.shellId);
 	}
 
 	@Test
@@ -103,29 +115,33 @@ public class ObservableSubmodelAggregatorTest {
 		public boolean deletedNotified = false;
 		public boolean updatedNotified = false;
 
+		public String shellId = "";
 		public String submodelId = "";
 
 		@Override
-		public void submodelCreated(String aasId, String submodelId) {
+		public void submodelCreated(String shellId, String submodelId) {
 			createdNotified = true;
 			deletedNotified = false;
 			updatedNotified = false;
+			this.shellId = shellId;
 			this.submodelId = submodelId;
 		}
 
 		@Override
-		public void submodelUpdated(String aasId, String submodelId) {
+		public void submodelUpdated(String shellId, String submodelId) {
 			createdNotified = false;
 			deletedNotified = false;
 			updatedNotified = true;
+			this.shellId = shellId;
 			this.submodelId = submodelId;
 		}
 
 		@Override
-		public void submodelDeleted(String aasId, String submodelId) {
+		public void submodelDeleted(String shellId, String submodelId) {
 			createdNotified = false;
 			deletedNotified = true;
 			updatedNotified = false;
+			this.shellId = shellId;
 			this.submodelId = submodelId;
 		}
 	}
