8a5f16118aebe7faf9eb2c3b93f82e8f67b623c8
Frank Schnicke <frank.schnicke@iese.fraunhofer.de>
2022-01-19 14:39:51 +0100

    Fixes JavaDoc errors
    Signed-off-by: Frank Schnicke <frank.schnicke@iese.fraunhofer.de>

--- src/main/java/org/eclipse/basyx/extensions/aas/aggregator/mqtt/MqttAASAggregator.java
+++ src/main/java/org/eclipse/basyx/extensions/aas/aggregator/mqtt/MqttAASAggregator.java
@@ -41,23 +41,22 @@ public class MqttAASAggregator implements IAASAggregator {
 	/**
 	 * Constructor for adding this MQTT extension on top of an AASAggregator
 	 * 
-	 * @param observedAASAggregator the underlying AAS Aggregator 
+	 * @param aasAggregatorToBeObserved the underlying AAS Aggregator 
 	 * @param serverEndpoint endpoint of mqtt broker
 	 * @param clientId unique client identifier
 	 * @throws MqttException
 	 */
-	public MqttAASAggregator(IAASAggregator observedAASAggregator, String serverEndpoint, String clientId) throws MqttException {
-		this(observedAASAggregator, serverEndpoint, clientId, new MqttDefaultFilePersistence());
+	public MqttAASAggregator(IAASAggregator aasAggregatorToBeObserved, String serverEndpoint, String clientId) throws MqttException {
+		this(aasAggregatorToBeObserved, serverEndpoint, clientId, new MqttDefaultFilePersistence());
 	}
 
 	/**
 	 * Constructor for adding this MQTT extension on top of an AASAggregator
 	 * 
-	 * @param observedAASAggregator the underlying AAS Aggregator 
+	 * @param aasAggregatorToBeObserved the underlying AAS Aggregator 
 	 * @param serverEndpoint endpoint of mqtt broker
 	 * @param clientId unique client identifier
-	 * @param user username for authentication with broker
-	 * @param pw password for authentication with broker
+
 	 * @throws MqttException
 	 */
 	public MqttAASAggregator(IAASAggregator aasAggregatorToBeObserved, String serverEndpoint, String clientId, MqttClientPersistence persistence) throws MqttException {
@@ -69,8 +68,12 @@ public class MqttAASAggregator implements IAASAggregator {
 	/**
 	 * Constructor for adding this MQTT extension on top of an AASAggregator
 	 * 
-	 * @param observedAASAggregator the underlying AAS Aggregator 
-	 * @param client already configured client
+	 * @param aasAggregatorToBeObserved the underlying AAS Aggregator 
+	 * @param serverEndpoint endpoint of mqtt broker
+	 * @param clientId unique client identifier
+	 * @param user username for authentication with broker
+	 * @param pw password for authentication with broker
+ 	 * @param pw password for authentication with broker
 	 * @throws MqttException
 	 */
 	public MqttAASAggregator(IAASAggregator aasAggregatorToBeObserved, String serverEndpoint, String clientId, String user, char[] pw) throws MqttException {
@@ -79,12 +82,11 @@ public class MqttAASAggregator implements IAASAggregator {
 
 	/**
 	 *
-	 * @param aasAggregatorToBeObserved
-	 * @param serverEndpoint
-	 * @param clientId
-	 * @param user
-	 * @param pw
-	 * @param persistence
+	 * @param aasAggregatorToBeObserved the underlying AAS Aggregator 
+	 * @param serverEndpoint endpoint of mqtt broker
+	 * @param clientId unique client identifier
+	 * @param user username for authentication with broker
+	 * @param persistence persistence level
 	 * @throws MqttException
 	 */
 	public MqttAASAggregator(IAASAggregator aasAggregatorToBeObserved, String serverEndpoint, String clientId, String user, char[] pw, MqttClientPersistence persistence) throws MqttException {
