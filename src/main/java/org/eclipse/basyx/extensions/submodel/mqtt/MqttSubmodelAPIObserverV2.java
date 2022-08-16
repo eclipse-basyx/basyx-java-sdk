e7e3c2d914fb6c367b963253389cfd82c5e8ab43
Mischa Siebert <mischa.siebert@objective-partner.com>
2022-08-16 14:20:26 +0200

    Implements addSubmodelElement as V2

---
+++ src/main/java/org/eclipse/basyx/extensions/submodel/mqtt/MqttSubmodelAPIObserverV2.java
@@ -0,0 +1,274 @@
+/*******************************************************************************
+* Copyright (C) 2021 the Eclipse BaSyx Authors
+*
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to
+ * the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+ * LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+ * OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+ * WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*
+* SPDX-License-Identifier: MIT
+******************************************************************************/
+package org.eclipse.basyx.extensions.submodel.mqtt;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import org.eclipse.basyx.extensions.shared.mqtt.MqttEventService;
+import org.eclipse.basyx.submodel.metamodel.api.identifier.IIdentifier;
+import org.eclipse.basyx.submodel.restapi.observing.ISubmodelAPIObserver;
+import org.eclipse.basyx.submodel.restapi.observing.ISubmodelAPIObserverV2;
+import org.eclipse.basyx.submodel.restapi.observing.ObservableSubmodelAPI;
+import org.eclipse.basyx.submodel.restapi.observing.ObservableSubmodelAPIV2;
+import org.eclipse.basyx.vab.coder.json.serialization.DefaultTypeFactory;
+import org.eclipse.basyx.vab.coder.json.serialization.GSONTools;
+import org.eclipse.basyx.vab.modelprovider.VABPathTools;
+import org.eclipse.paho.client.mqttv3.MqttClient;
+import org.eclipse.paho.client.mqttv3.MqttClientPersistence;
+import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
+import org.eclipse.paho.client.mqttv3.MqttException;
+import org.eclipse.paho.client.mqttv3.persist.MqttDefaultFilePersistence;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Implementation of {@link ISubmodelAPIObserver} Triggers MQTT events for
+ * different CRUD operations on the submodel.
+ * 
+ * @author conradi, danish
+ *
+ */
+public class MqttSubmodelAPIObserverV2 extends MqttEventService implements ISubmodelAPIObserverV2 {
+	private static Logger logger = LoggerFactory.getLogger(MqttSubmodelAPIObserverV2.class);
+
+	// Submodel Element whitelist for filtering
+	protected boolean useWhitelist = false;
+	protected Set<String> whitelist = new HashSet<>();
+	
+	private IIdentifier aasIdentifier;
+	private IIdentifier submodelIdentifier;
+
+	/**
+	 * Constructor for adding this MQTT extension on top of another SubmodelAPI
+	 * 
+	 * @param client
+	 *            An already connected mqtt client
+	 * @param aasId 
+	 * @param submodelIdentifier
+	 * 
+	 * @throws MqttException
+	 */
+	public MqttSubmodelAPIObserverV2(MqttClient client, IIdentifier aasId, IIdentifier submodelIdentifier) throws MqttException {
+		super(client);
+		
+		connectMqttClientIfRequired();
+		
+		this.aasIdentifier = aasId;
+		this.submodelIdentifier = submodelIdentifier;
+		
+		sendMqttMessage(MqttSubmodelAPIHelper.TOPIC_CREATESUBMODEL, this.submodelIdentifier.getId());
+	}
+	
+	/**
+	 * Constructor for adding this MQTT extension on top of another SubmodelAPI
+	 * 
+	 * @param client
+	 *            An already connected mqtt client
+	 * @param aasId 
+	 * @param submodelIdentifier
+	 * @param options
+	 * 
+	 * @throws MqttException
+	 */
+	public MqttSubmodelAPIObserverV2(MqttClient client, IIdentifier aasId, IIdentifier submodelIdentifier, MqttConnectOptions options) throws MqttException {
+		super(client);
+		
+		connectMqttClientIfRequired(options);
+		
+		this.aasIdentifier = aasId;
+		this.submodelIdentifier = submodelIdentifier;
+		
+		sendMqttMessage(MqttSubmodelAPIHelper.TOPIC_CREATESUBMODEL, this.submodelIdentifier.getId());
+	}
+	
+	/**
+	 * Constructor for adding this MQTT extension on top of another SubmodelAPI
+	 * 
+	 * @param observedAPI
+	 *            The underlying submodelAPI
+	 * @throws MqttException
+	 * 
+	 * @deprecated This constructor is deprecated please use {@link #MqttSubmodelAPIObserverV2(MqttClient, IIdentifier, IIdentifier)} instead.
+	 */
+	@Deprecated
+	public MqttSubmodelAPIObserverV2(ObservableSubmodelAPIV2 observedAPI, String serverEndpoint, String clientId) throws MqttException {
+		this(observedAPI, serverEndpoint, clientId, new MqttDefaultFilePersistence());
+	}
+
+	/**
+	 * Constructor for adding this MQTT extension on top of another SubmodelAPI with
+	 * a custom persistence strategy
+	 * 
+	 * @deprecated This constructor is deprecated please use {@link #MqttSubmodelAPIObserverV2(MqttClient, IIdentifier, IIdentifier)} instead.
+	 */
+	@Deprecated
+	public MqttSubmodelAPIObserverV2(ObservableSubmodelAPIV2 observedAPI, String brokerEndpoint, String clientId, MqttClientPersistence persistence) throws MqttException {
+		this(new MqttClient(brokerEndpoint, clientId, persistence), MqttSubmodelAPIHelperV2.getAASId(observedAPI), MqttSubmodelAPIHelperV2.getSubmodelId(observedAPI));
+		logger.info("Create new MQTT submodel for endpoint " + brokerEndpoint);
+		
+		observedAPI.addObserver(this);
+		
+		sendMqttMessage(MqttSubmodelAPIHelper.TOPIC_CREATESUBMODEL, this.submodelIdentifier.getId());
+	}
+
+	/**
+	 * Constructor for adding this MQTT extension on top of another SubmodelAPI
+	 * 
+	 * @param observedAPI
+	 *            The underlying submodelAPI
+	 * @throws MqttException
+	 * 
+	 * @deprecated This constructor is deprecated please use {@link #MqttSubmodelAPIObserverV2(MqttClient, IIdentifier, IIdentifier, MqttConnectOptions)} instead.
+	 */
+	@Deprecated
+	public MqttSubmodelAPIObserverV2(ObservableSubmodelAPIV2 observedAPI, String serverEndpoint, String clientId, String user, char[] pw) throws MqttException {
+		this(observedAPI, serverEndpoint, clientId, user, pw, new MqttDefaultFilePersistence());
+	}
+
+	/**
+	 * Constructor for adding this MQTT extension on top of another SubmodelAPI with
+	 * credentials and persistency strategy
+	 * 
+	 * @deprecated This constructor is deprecated please use {@link #MqttSubmodelAPIObserverV2(MqttClient, IIdentifier, IIdentifier, MqttConnectOptions)} instead.
+	 */
+	@Deprecated
+	public MqttSubmodelAPIObserverV2(ObservableSubmodelAPIV2 observedAPI, String serverEndpoint, String clientId, String user, char[] pw, MqttClientPersistence persistence) throws MqttException {
+		this(new MqttClient(serverEndpoint, clientId, persistence), MqttSubmodelAPIHelperV2.getAASId(observedAPI), MqttSubmodelAPIHelperV2.getSubmodelId(observedAPI), MqttSubmodelAPIHelper.getMqttConnectOptions(user, pw));
+		logger.info("Create new MQTT submodel for endpoint " + serverEndpoint);
+		
+		observedAPI.addObserver(this);
+		
+		sendMqttMessage(MqttSubmodelAPIHelper.TOPIC_CREATESUBMODEL, this.submodelIdentifier.getId());
+	}
+
+	/**
+	 * Constructor for adding this MQTT extension on top of another SubmodelAPI.
+	 * 
+	 * @param observedAPI
+	 *            The underlying submodelAPI
+	 * @param client
+	 *            An already connected mqtt client
+	 * @throws MqttException
+	 * 
+	 * @deprecated This constructor is deprecated please use {@link #MqttSubmodelAPIObserverV2(MqttClient, IIdentifier, IIdentifier)} instead.
+	 */
+	@Deprecated
+	public MqttSubmodelAPIObserverV2(ObservableSubmodelAPIV2 observedAPI, MqttClient client) throws MqttException {
+		this(client, MqttSubmodelAPIHelperV2.getAASId(observedAPI), MqttSubmodelAPIHelperV2.getSubmodelId(observedAPI));
+		
+		observedAPI.addObserver(this);
+		
+		sendMqttMessage(MqttSubmodelAPIHelper.TOPIC_CREATESUBMODEL, this.submodelIdentifier.getId());
+	}
+	
+	private void connectMqttClientIfRequired() throws MqttException {
+		if(!mqttClient.isConnected()) {
+			mqttClient.connect();
+		}
+	}
+	
+	private void connectMqttClientIfRequired(MqttConnectOptions options) throws MqttException {
+		if(!mqttClient.isConnected()) {
+			mqttClient.connect(options);
+		}
+	}
+
+	/**
+	 * Adds a submodel element to the filter whitelist. Can also be a path for
+	 * nested submodel elements.
+	 * 
+	 * @param shortId
+	 */
+	public void observeSubmodelElement(String shortId) {
+		whitelist.add(VABPathTools.stripSlashes(shortId));
+	}
+
+	/**
+	 * Sets a new filter whitelist.
+	 * 
+	 * @param shortIds
+	 */
+	public void setWhitelist(Set<String> shortIds) {
+		this.whitelist.clear();
+		for (String entry : shortIds) {
+			this.whitelist.add(VABPathTools.stripSlashes(entry));
+		}
+	}
+
+	/**
+	 * Disables the submodel element filter whitelist
+	 * 
+	 */
+	public void disableWhitelist() {
+		useWhitelist = false;
+	}
+
+	/**
+	 * Enables the submodel element filter whitelist
+	 * 
+	 */
+	public void enableWhitelist() {
+		useWhitelist = true;
+	}
+
+	@Override
+	public void elementAdded(String idShortPath, Object newValue) {
+		if (filter(idShortPath)) {
+			sendMqttMessage(MqttSubmodelAPIHelperV2.createCreateSubmodelElementTopic(idShortPath), serializePayload(newValue));
+		}
+	}
+
+	@Override
+	public void elementDeleted(String idShortPath) {
+		if (filter(idShortPath)) {
+			sendMqttMessage(MqttSubmodelAPIHelper.TOPIC_DELETEELEMENT, getCombinedMessage(aasIdentifier.getId(), submodelIdentifier.getId(), idShortPath));
+		}
+	}
+
+	@Override
+	public void elementUpdated(String idShortPath, Object newValue) {
+		if (filter(idShortPath)) {
+			sendMqttMessage(MqttSubmodelAPIHelper.TOPIC_UPDATEELEMENT, getCombinedMessage(aasIdentifier.getId(), submodelIdentifier.getId(), idShortPath));
+		}
+	}
+
+	public static String getCombinedMessage(String aasId, String submodelId, String elementPart) {
+		elementPart = VABPathTools.stripSlashes(elementPart);
+		return "(" + aasId + "," + submodelId + "," + elementPart + ")";
+	}
+
+	private boolean filter(String idShort) {
+		idShort = VABPathTools.stripSlashes(idShort);
+		return !useWhitelist || whitelist.contains(idShort);
+	}
+
+	private String serializePayload(Object payload) {
+		GSONTools tools = new GSONTools(new DefaultTypeFactory(), false, false);
+		
+		return tools.serialize(payload);
+	}
+	
+}
---
+++ src/test/java/org/eclipse/basyx/testsuite/regression/extensions/submodel/mqtt/MqttSubmodelAPIObserverTestV2.java
@@ -0,0 +1,178 @@
+/*******************************************************************************
+ * Copyright (C) 2021 the Eclipse BaSyx Authors
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to
+ * the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+ * LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+ * OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+ * WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ * 
+ * SPDX-License-Identifier: MIT
+ ******************************************************************************/
+package org.eclipse.basyx.testsuite.regression.extensions.submodel.mqtt;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+
+import java.io.IOException;
+
+import org.eclipse.basyx.extensions.submodel.mqtt.MqttDecoratingSubmodelAPIFactory;
+import org.eclipse.basyx.extensions.submodel.mqtt.MqttDecoratingSubmodelAPIFactoryV2;
+import org.eclipse.basyx.extensions.submodel.mqtt.MqttSubmodelAPIHelper;
+import org.eclipse.basyx.extensions.submodel.mqtt.MqttSubmodelAPIHelperV2;
+import org.eclipse.basyx.extensions.submodel.mqtt.MqttSubmodelAPIObserver;
+import org.eclipse.basyx.submodel.metamodel.api.identifier.IdentifierType;
+import org.eclipse.basyx.submodel.metamodel.api.reference.enums.KeyElements;
+import org.eclipse.basyx.submodel.metamodel.map.Submodel;
+import org.eclipse.basyx.submodel.metamodel.map.identifier.Identifier;
+import org.eclipse.basyx.submodel.metamodel.map.reference.Key;
+import org.eclipse.basyx.submodel.metamodel.map.reference.Reference;
+import org.eclipse.basyx.submodel.metamodel.map.submodelelement.SubmodelElementCollection;
+import org.eclipse.basyx.submodel.metamodel.map.submodelelement.dataelement.property.Property;
+import org.eclipse.basyx.submodel.restapi.api.ISubmodelAPI;
+import org.eclipse.basyx.submodel.restapi.vab.VABSubmodelAPIFactory;
+import org.eclipse.basyx.testsuite.regression.extensions.shared.mqtt.MqttTestListener;
+import org.eclipse.basyx.vab.coder.json.serialization.DefaultTypeFactory;
+import org.eclipse.basyx.vab.coder.json.serialization.GSONTools;
+import org.eclipse.paho.client.mqttv3.MqttClient;
+import org.eclipse.paho.client.mqttv3.MqttException;
+import org.eclipse.paho.client.mqttv3.persist.MqttDefaultFilePersistence;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import io.moquette.broker.Server;
+import io.moquette.broker.config.ClasspathResourceLoader;
+import io.moquette.broker.config.IConfig;
+import io.moquette.broker.config.IResourceLoader;
+import io.moquette.broker.config.ResourceLoaderConfig;
+
+/**
+ * Test for MqttSubmodelAPIObserver
+ * 
+ * @author espen, conradi, danish
+ *
+ */
+public class MqttSubmodelAPIObserverTestV2 {
+	private static final String CLIENT_ID = "testClient";
+	private static final String SERVER_URI = "tcp://localhost:1884";
+	private static final String AASID = "testaasid";
+	private static final String SUBMODELID = "testsubmodelid";
+
+	private static Server mqttBroker;
+	private static ISubmodelAPI observableAPI;
+	private MqttTestListener listener;
+	
+	private static Submodel submodel;
+
+	/**
+	 * Sets up the MQTT broker and submodelAPI for tests
+	 */
+	@BeforeClass
+	public static void setUpClass() throws MqttException, IOException {
+		// Start MQTT broker
+		mqttBroker = new Server();
+		IResourceLoader classpathLoader = new ClasspathResourceLoader();
+		final IConfig classPathConfig = new ResourceLoaderConfig(classpathLoader);
+		mqttBroker.startServer(classPathConfig);
+
+		// Create submodel
+		submodel = new Submodel(SUBMODELID, new Identifier(IdentifierType.CUSTOM, SUBMODELID));
+		Reference parentRef = new Reference(new Key(KeyElements.ASSETADMINISTRATIONSHELL, true, AASID, IdentifierType.IRDI));
+		submodel.setParent(parentRef);
+		
+		observableAPI = createObservableSubmodelAPI();
+	}
+
+	private static ISubmodelAPI createObservableSubmodelAPI() throws MqttException {
+		return new MqttDecoratingSubmodelAPIFactoryV2(new VABSubmodelAPIFactory(), new MqttClient(SERVER_URI, CLIENT_ID, new MqttDefaultFilePersistence())).getSubmodelAPI(submodel);
+	}
+
+	@AfterClass
+	public static void tearDownClass() {
+		mqttBroker.stopServer();
+	}
+
+	@Before
+	public void setUp() {
+		listener = new MqttTestListener();
+		mqttBroker.addInterceptHandler(listener);
+	}
+
+	@After
+	public void tearDown() {
+		mqttBroker.removeInterceptHandler(listener);
+	}
+
+	@Test
+	public void testAddSubmodelElement() throws InterruptedException {
+		String elemIdShort = "testAddProp";
+		Property prop = new Property(true);
+		prop.setIdShort(elemIdShort);
+		observableAPI.addSubmodelElement(prop);
+
+		assertEquals(prop, deserializePayload(listener.lastPayload));
+		assertEquals(MqttSubmodelAPIHelperV2.createCreateSubmodelElementTopic(elemIdShort), listener.lastTopic);
+	}
+
+	@Test
+	public void testAddNestedSubmodelElement() {
+		String idShortPath = "/testColl/testAddProp/";
+		SubmodelElementCollection coll = new SubmodelElementCollection();
+		coll.setIdShort("testColl");
+		observableAPI.addSubmodelElement(coll);
+
+		Property prop = new Property(true);
+		prop.setIdShort("testAddProp");
+		observableAPI.addSubmodelElement(idShortPath, prop);
+
+		assertEquals(prop, deserializePayload(listener.lastPayload));
+		assertEquals(MqttSubmodelAPIHelperV2.createCreateSubmodelElementTopic(idShortPath), listener.lastTopic);
+	}
+
+	@Test
+	public void testDeleteSubmodelElement() {
+		String idShortPath = "/testDeleteProp";
+		Property prop = new Property(true);
+		prop.setIdShort("testDeleteProp");
+		observableAPI.addSubmodelElement(prop);
+		observableAPI.deleteSubmodelElement(idShortPath);
+
+		assertEquals(MqttSubmodelAPIObserver.getCombinedMessage(AASID, SUBMODELID, idShortPath), listener.lastPayload);
+		assertEquals(MqttSubmodelAPIHelper.TOPIC_DELETEELEMENT, listener.lastTopic);
+	}
+
+	@Test
+	public void testUpdateSubmodelElement() {
+		String idShortPath = "testUpdateProp";
+		Property prop = new Property(true);
+		prop.setIdShort(idShortPath);
+		observableAPI.addSubmodelElement(prop);
+		observableAPI.updateSubmodelElement(idShortPath, false);
+
+		assertFalse((boolean) observableAPI.getSubmodelElementValue(idShortPath));
+		assertEquals(MqttSubmodelAPIObserver.getCombinedMessage(AASID, SUBMODELID, idShortPath), listener.lastPayload);
+		assertEquals(MqttSubmodelAPIHelper.TOPIC_UPDATEELEMENT, listener.lastTopic);
+	}
+	
+	private Object deserializePayload(String payload) {
+		GSONTools tools = new GSONTools(new DefaultTypeFactory(), false, false);
+		
+		return tools.deserialize(payload);
+	}
+}
