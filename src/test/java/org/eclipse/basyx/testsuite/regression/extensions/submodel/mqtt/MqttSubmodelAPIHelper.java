cc5ff38cd03af6f1ebbdeef44570d8dc726a7f91
Jannik Fried <Jannik.Fried@iese.fraunhofer.de>
2022-09-13 12:01:42 +0200

    Updates test according to code review
    Signed-off-by: Jannik Fried <Jannik.Fried@iese.fraunhofer.de>

--- src/test/java/org/eclipse/basyx/testsuite/regression/extensions/submodel/mqtt/MqttSubmodelAPIHelper.java
+++ src/test/java/org/eclipse/basyx/testsuite/regression/extensions/submodel/mqtt/MqttSubmodelAPIHelper.java
@@ -9,9 +9,6 @@
  ******************************************************************************/
 package org.eclipse.basyx.testsuite.regression.extensions.submodel.mqtt;
 
-import static org.eclipse.basyx.extensions.submodel.mqtt.MqttSubmodelAPIHelper.getAASId;
-import static org.junit.Assert.assertNull;
-
 import java.io.IOException;
 
 import org.eclipse.basyx.extensions.submodel.mqtt.MqttDecoratingSubmodelAPIFactory;
@@ -19,13 +16,11 @@ import org.eclipse.basyx.submodel.metamodel.api.identifier.IdentifierType;
 import org.eclipse.basyx.submodel.metamodel.map.Submodel;
 import org.eclipse.basyx.submodel.metamodel.map.identifier.Identifier;
 import org.eclipse.basyx.submodel.restapi.api.ISubmodelAPI;
-import org.eclipse.basyx.submodel.restapi.observing.ObservableSubmodelAPI;
 import org.eclipse.basyx.submodel.restapi.vab.VABSubmodelAPIFactory;
 import org.eclipse.paho.client.mqttv3.MqttClient;
 import org.eclipse.paho.client.mqttv3.MqttException;
 import org.eclipse.paho.client.mqttv3.persist.MqttDefaultFilePersistence;
 import org.junit.AfterClass;
-import org.junit.BeforeClass;
 import org.junit.Test;
 
 import io.moquette.broker.Server;
@@ -39,37 +34,39 @@ public class MqttSubmodelAPIHelper {
 	private static final String SERVER_URI = "tcp://localhost:1884";
 	private static final String SUBMODELID = "testsubmodelid";
 
-	private static ISubmodelAPI observableAPI;
-
 	private static Submodel submodel;
 	private static Server mqttBroker;
-	private static ObservableSubmodelAPI observedApi;
 
-	/**
-	 * Sets up the MQTT broker and submodelAPI for tests
-	 */
-	@BeforeClass
-	public static void setUpClass() throws MqttException, IOException {
-		// Start MQTT broker
-		mqttBroker = new Server();
+	@Test
+	public void getAASIdReturnsNull() {
+		mqttBroker = startMqttBroker();
 		IResourceLoader classpathLoader = new ClasspathResourceLoader();
 		final IConfig classPathConfig = new ResourceLoaderConfig(classpathLoader);
-		mqttBroker.startServer(classPathConfig);
+		try {
+			mqttBroker.startServer(classPathConfig);
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
 
-		// Create submodel
-		submodel = new Submodel(SUBMODELID, new Identifier(IdentifierType.CUSTOM, SUBMODELID));
+		submodel = createSubmodel();
 
-		observableAPI = createObservableSubmodelAPI();
-		observedApi = new ObservableSubmodelAPI(observableAPI);
+		try {
+			createObservableSubmodelAPI();
+		} catch (MqttException e) {
+			throw new RuntimeException(e);
+		}
 	}
 
 	private static ISubmodelAPI createObservableSubmodelAPI() throws MqttException {
 		return new MqttDecoratingSubmodelAPIFactory(new VABSubmodelAPIFactory(), new MqttClient(SERVER_URI, CLIENT_ID, new MqttDefaultFilePersistence())).getSubmodelAPI(submodel);
 	}
 
-	@Test
-	public void getAASIdReturnsNull() {
-		assertNull(getAASId(observedApi));
+	private Submodel createSubmodel() {
+		return new Submodel(SUBMODELID, new Identifier(IdentifierType.CUSTOM, SUBMODELID));
+	}
+
+	private Server startMqttBroker() {
+		return new Server();
 	}
 
 	@AfterClass
