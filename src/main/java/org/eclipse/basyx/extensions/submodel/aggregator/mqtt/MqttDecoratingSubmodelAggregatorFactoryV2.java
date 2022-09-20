470578645e0f537bb179fc3f4eb8d771bad81d38
Mischa Siebert <mischa.siebert@objective-partner.com>
2022-09-20 15:14:50 +0200

    implement V2

---
+++ src/main/java/org/eclipse/basyx/extensions/submodel/aggregator/mqtt/MqttDecoratingSubmodelAggregatorFactoryV2.java
@@ -0,0 +1,68 @@
+/*******************************************************************************
+ * Copyright (C) 2021 the Eclipse BaSyx Authors
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to
+ * the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+ * LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+ * OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+ * WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ * 
+ * SPDX-License-Identifier: MIT
+ ******************************************************************************/
+
+package org.eclipse.basyx.extensions.submodel.aggregator.mqtt;
+
+import java.security.ProviderException;
+
+import org.eclipse.basyx.submodel.aggregator.api.ISubmodelAggregator;
+import org.eclipse.basyx.submodel.aggregator.api.ISubmodelAggregatorFactory;
+import org.eclipse.basyx.submodel.aggregator.observing.ObservableSubmodelAggregator;
+import org.eclipse.basyx.submodel.aggregator.observing.ObservableSubmodelAggregatorV2;
+import org.eclipse.paho.client.mqttv3.MqttClient;
+import org.eclipse.paho.client.mqttv3.MqttException;
+
+/**
+ * Factory decorating SubmodelAggregator with MQTT events by wrapping an
+ * ISubmodelAggregatorFactory
+ * 
+ * @author fried
+ */
+public class MqttDecoratingSubmodelAggregatorFactoryV2 implements ISubmodelAggregatorFactory {
+	private ISubmodelAggregatorFactory submodelAggregatorFactory;
+	private MqttClient mqttClient;
+
+	private ObservableSubmodelAggregatorV2 observedSubmodelAggregator;
+	protected MqttSubmodelAggregatorObserverV2 observer;
+
+	public MqttDecoratingSubmodelAggregatorFactoryV2(ISubmodelAggregatorFactory submodelAggregatorFactory, MqttClient mqttClient) {
+		this.submodelAggregatorFactory = submodelAggregatorFactory;
+		this.mqttClient = mqttClient;
+	}
+
+	@Override
+	public ISubmodelAggregator create() {
+		try {
+			ISubmodelAggregator aggregator = submodelAggregatorFactory.create();
+			observedSubmodelAggregator = new ObservableSubmodelAggregatorV2(aggregator);
+			observer = new MqttSubmodelAggregatorObserverV2(mqttClient);
+			observedSubmodelAggregator.addObserver(observer);
+			return observedSubmodelAggregator;
+		} catch (MqttException e) {
+			throw new ProviderException(e);
+		}
+	}
+
+}
