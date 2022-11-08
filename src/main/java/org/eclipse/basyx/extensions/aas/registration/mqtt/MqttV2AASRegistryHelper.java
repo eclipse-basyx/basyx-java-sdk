e333f6da43341351660df323a1e26c86a2c92eac
Mischa Siebert <mischa.siebert@objective-partner.com>
2022-11-08 17:01:04 +0100

    remove unused method

--- src/main/java/org/eclipse/basyx/extensions/aas/registration/mqtt/MqttV2AASRegistryHelper.java
+++ src/main/java/org/eclipse/basyx/extensions/aas/registration/mqtt/MqttV2AASRegistryHelper.java
@@ -44,17 +44,6 @@ public class MqttV2AASRegistryHelper {
 	private static final String CREATED = "created";
 	private static final String UPDATED = "updated";
 	private static final String DELETED = "deleted";
-
-	/**
-	 * This function is for creating a message payload representing a descriptor
-	 * change of a submodel of a specific AAS.
-	 * 
-	 * @param aasId
-	 * @param smId
-	 */
-	public static String createSubmodelDescriptorOfAASChangedPayload(IIdentifier aasId, IIdentifier smId) {
-		return "(" + aasId.getId() + "," + smId.getId() + ")";
-	}
 	
 	/**
 	 * This method creates a Mqtt topic like /aas-registry/default/shells/created
