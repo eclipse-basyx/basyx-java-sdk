f1908cf2af59874f292668320f555635ac802dad
Mohammad Ghazanfar Ali Danish <62088117+mdanish98@users.noreply.github.com>
2022-11-22 14:26:50 +0100

    Implements feature for AAS Server Property Delegation (#163)
    * Implements thumbnail support feature
    
    Description:
    - Adds features to set and retrieve thumnail from/to package
    - Adds testcases
    
    * Removes empty lines
    
    Signed-off-by: Mohammad Ghazanfar Ali Danish <ghazanfar.danish@iese.fraunhofer.de>
    
    * Refactors code
    
    Description:
    - The Thumbnail should be passed as InputStream
    - Adds a thumbnail variant for the constructor utilizing AASEnv
    - Updates TCs to adapt these changes
    
    * Refactors code
    
    Description:
    - Adds Testcase to check the content of thumbnail
    - Rename TEST_CONTENT to THUMBNAIL
    
    Signed-off-by: Mohammad Ghazanfar Ali Danish <ghazanfar.danish@iese.fraunhofer.de>
    
    * Implements feature for AAS Server Property Delegation
    
    Description:
    - Adds Testcase to cover the implementations
    
    Signed-off-by: Mohammad Ghazanfar Ali Danish <ghazanfar.danish@iese.fraunhofer.de>
    
    * Refactors code and adds missing TCs
    
    Description:
    - Moves PropertyDelegationManager from shared
    - Adds javadoc for public methods
    - Adds missing tests for DelegatingSubmodelAPI
    
    Signed-off-by: Mohammad Ghazanfar Ali Danish <ghazanfar.danish@iese.fraunhofer.de>
    
    * Adds TCs for DelegatingSubmodelAPI
    
    Description:
    - Adds mock-server dependency
    - Adds a helper class DelegationTestHelper
    - Removes unnecessary TCs from TestPropertyDelegationManager
    
    Signed-off-by: Mohammad Ghazanfar Ali Danish <ghazanfar.danish@iese.fraunhofer.de>
    
    * Fixes build failure
    
    Signed-off-by: Mohammad Ghazanfar Ali Danish <ghazanfar.danish@iese.fraunhofer.de>
    
    * Refactors code
    
    Description:
    - Modifies methods and variables from protected to public in
    DelegationTestHelper
    - Removes delay defined in the mockServer
    
    Signed-off-by: Mohammad Ghazanfar Ali Danish <ghazanfar.danish@iese.fraunhofer.de>
    
    Signed-off-by: Mohammad Ghazanfar Ali Danish <ghazanfar.danish@iese.fraunhofer.de>
    Co-authored-by: Frank Schnicke <frank.schnicke@iese.fraunhofer.de>

---
+++ src/main/java/org/eclipse/basyx/extensions/submodel/delegation/PropertyDelegationManager.java
@@ -0,0 +1,149 @@
+/*******************************************************************************
+ * Copyright (C) 2022 the Eclipse BaSyx Authors
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to
+ * the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+ * LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+ * OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+ * WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ *
+ * SPDX-License-Identifier: MIT
+ ******************************************************************************/
+package org.eclipse.basyx.extensions.submodel.delegation;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Collection;
+import java.util.Optional;
+
+import org.eclipse.basyx.submodel.metamodel.api.ISubmodel;
+import org.eclipse.basyx.submodel.metamodel.api.qualifier.qualifiable.IConstraint;
+import org.eclipse.basyx.submodel.metamodel.api.qualifier.qualifiable.IQualifier;
+import org.eclipse.basyx.submodel.metamodel.map.qualifier.qualifiable.Qualifier;
+import org.eclipse.basyx.submodel.metamodel.map.submodelelement.SubmodelElement;
+import org.eclipse.basyx.submodel.metamodel.map.submodelelement.SubmodelElementCollection;
+import org.eclipse.basyx.submodel.metamodel.map.submodelelement.dataelement.property.AASLambdaPropertyHelper;
+import org.eclipse.basyx.submodel.metamodel.map.submodelelement.dataelement.property.Property;
+import org.eclipse.basyx.submodel.metamodel.map.submodelelement.dataelement.property.valuetype.ValueType;
+import org.eclipse.basyx.vab.exception.provider.ProviderException;
+import org.eclipse.basyx.vab.modelprovider.api.IModelProvider;
+import org.eclipse.basyx.vab.protocol.api.ConnectorFactory;
+
+/**
+ * Checks and manages Property with delegation
+ * 
+ * @author danish
+ */
+public class PropertyDelegationManager {
+	private static final String DELEGATION_TYPE = "delegatedTo";
+
+	private ConnectorFactory connectorProvider;
+
+	/**
+	 * Initializes the connectorProvider, which is used for communicating
+	 * with the delegated endpoint
+	 * 
+	 * @param connectorProvider
+	 */
+	public PropertyDelegationManager(ConnectorFactory connectorProvider) {
+		this.connectorProvider = connectorProvider;
+	}
+
+	/**
+	 * Adds delegation mechanism to the delegated property contained in the 
+	 * provided submodel
+	 * 
+	 * @param submodel
+	 */
+	public void handleSubmodel(ISubmodel submodel) {
+		submodel.getSubmodelElements().values().stream().map(SubmodelElement.class::cast).forEach(this::handleSubmodelElement);
+	}
+	
+	/**
+	 * Adds delegation mechanism to the delegated property contained in the
+	 * provided submodel element
+	 * 
+	 * @param submodelElement
+	 */
+	public void handleSubmodelElement(SubmodelElement submodelElement) {
+		if (Property.isProperty(submodelElement)) {
+			handleProperty(Property.createAsFacade(submodelElement));
+		} else if (SubmodelElementCollection.isSubmodelElementCollection(submodelElement)) {
+			handeSubmodelElementCollection(SubmodelElementCollection.createAsFacade(submodelElement));
+		}
+	}
+
+	/**
+	 * Creates a qualifier with a delegatedTo type
+	 * 
+	 * @param delegationUrl
+	 *            - URL of the delegation endpoint
+	 */
+	public static IQualifier createDelegationQualifier(String delegationUrl) {
+		Qualifier qualifier = new Qualifier(DELEGATION_TYPE, ValueType.String);
+		qualifier.setValue(delegationUrl);
+
+		return qualifier;
+	}
+	
+	/**
+	 * Checks if the provided qualifier is of delegatedTo type
+	 * 
+	 * @param iConstraint
+	 * @return 
+	 */
+	public static boolean isDelegationQualifier(IConstraint iConstraint) {
+		return iConstraint instanceof Qualifier && ((Qualifier) iConstraint).getType().equals(DELEGATION_TYPE);
+	}
+
+	private void handeSubmodelElementCollection(SubmodelElementCollection smc) {
+		smc.getSubmodelElements().values().stream().map(SubmodelElement.class::cast)
+				.forEach(this::handleSubmodelElement);
+	}
+
+	private void handleProperty(Property property) {
+		Collection<IConstraint> qualifiers = property.getQualifiers();
+		Optional<IConstraint> optionalConstraint = qualifiers.stream().filter(PropertyDelegationManager::isDelegationQualifier).findAny();
+
+		if (optionalConstraint.isEmpty())
+			return;
+
+		Qualifier delegationQualifier = (Qualifier) optionalConstraint.get();
+		configureDelegationProperty(property, delegationQualifier);
+	}
+
+	private void configureDelegationProperty(SubmodelElement submodelElement, IConstraint iConstraint) {
+		String delegatedTo = ((Qualifier) iConstraint).getValue().toString();
+
+		AASLambdaPropertyHelper.setLambdaValue((Property) submodelElement, () -> getPropertyValue(delegatedTo), null);
+	}
+
+	private Object getPropertyValue(String delegatedTo) {
+		try {
+			URL url = new URL(delegatedTo);
+			String address = createAddressFromURL(url);
+			IModelProvider connector = connectorProvider.create(address);
+			return connector.getValue(url.getPath());
+		} catch (MalformedURLException e) {
+			throw new ProviderException(e);
+		}
+	}
+
+	private String createAddressFromURL(URL url) {
+		return url.getProtocol() + "://" + url.getHost() + ":"
+				+ (url.getPort() != -1 ? Integer.toString(url.getPort()) : "");
+	}
+}
