0837359f8a4250145ac746ec6b2e4befd52ab715
FlorianWege-IESE <florian.wege@iese.fraunhofer.de>
2022-12-08 13:45:12 +0100

    Remove unused missionAuthority field in GrantedAuthorityInhibitException
    Signed-off-by: FlorianWege-IESE <florian.wege@iese.fraunhofer.de>

--- src/main/java/org/eclipse/basyx/extensions/shared/authorization/internal/GrantedAuthorityInhibitException.java
+++ src/main/java/org/eclipse/basyx/extensions/shared/authorization/internal/GrantedAuthorityInhibitException.java
@@ -31,12 +31,8 @@ package org.eclipse.basyx.extensions.shared.authorization.internal;
  * @author wege
  */
 public class GrantedAuthorityInhibitException extends InhibitException {
-	private final String missingAuthority;
-
 	public GrantedAuthorityInhibitException(final String missingAuthority) {
 		super("no authority " + missingAuthority);
-
-		this.missingAuthority = missingAuthority;
 	}
 
 	@Override
