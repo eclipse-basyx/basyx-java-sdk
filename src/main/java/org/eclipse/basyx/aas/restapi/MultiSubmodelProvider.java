8b5fa6aa353eac51708de69ebb9bac160180e99c
Frank Schnicke <77283144+FrankSchnicke@users.noreply.github.com>
2023-03-21 07:50:21 +0100

    Fixes HTTP Test (#250)
    Signed-off-by: Frank Schnicke <frank.schnicke@iese.fraunhofer.de>

--- src/main/java/org/eclipse/basyx/aas/restapi/MultiSubmodelProvider.java
+++ src/main/java/org/eclipse/basyx/aas/restapi/MultiSubmodelProvider.java
@@ -395,7 +395,7 @@ public class MultiSubmodelProvider implements IModelProvider {
 
 	private boolean isPropertyPathFileUploadPath(String propertyPath) {
 		String[] propertyPathParts = VABPathTools.splitPath(propertyPath);
-		boolean isFileUpload = VABPathTools.buildPath(propertyPathParts, 3).equals("upload");
+		boolean isFileUpload = VABPathTools.buildPath(propertyPathParts, 4).equals(UPLOAD);
 		return isFileUpload;
 	}
 
