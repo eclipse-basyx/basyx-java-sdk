2fd6dd9d28abdaccf385ed6ac9ecd03beaa435ea
Sebastian Eicke <134835859+seicke-harting@users.noreply.github.com>
2023-07-14 12:00:44 +0200

    Add (further) setIdentification Method to ConceptDescription

--- src/main/java/org/eclipse/basyx/submodel/metamodel/map/parts/ConceptDescription.java
+++ src/main/java/org/eclipse/basyx/submodel/metamodel/map/parts/ConceptDescription.java
@@ -143,6 +143,10 @@ public class ConceptDescription extends VABModelMap<Object> implements IConceptD
 		Identifiable.createAsFacade(this, getKeyElement()).setAdministration(information);
 	}
 
+	public void setIdentification(IIdentifier id) {
+		setIdentification(id.getIdType(), id.getId());
+	}
+
 	public void setIdentification(IdentifierType idType, String id) {
 		Identifiable.createAsFacadeNonStrict(this, getKeyElement()).setIdentification(idType, id);
 	}
